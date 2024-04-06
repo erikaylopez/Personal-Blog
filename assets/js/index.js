defc251fbb397899caf39efdf44e8c89407004f0
erikaylopez <eylopez8686@gmail.com>
2024-04-06 15:51:21 -0600

    changes to css

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+const username = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const submitButton = document.getElementById('submit');
+
+// submitButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+  function savelastPost() {
+
+    const lastPost = {
+        username: username.value,
+        title: title.value,
+       
+      };
+
+      localStorage.setItem('lastPost', JSON.stringify(lastPost));
+ 
+}
+  
+function renderLastPost() {
+    const lastPost = JSON.parse(localStorage.getItem('post'));
+    
+    if (lastPost !== null) {
+      document.getElementById('.username').innerHTML = lastPost.username;
+      document.getElementById('.title').innerHTML = lastPost.title;
+      document.getElementById('.content').innerHTML = lastPost.content;  
+    }
+  }
+
+  submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    savelastPost();
+    renderLastPost();
+  });
+  
+  function init() {
+    // When the init function is executed, the code inside renderLastGrade function will also execute
+    renderLastPost();
+  }
+  init();
